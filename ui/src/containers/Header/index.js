1480a1e636e36aa6021ceca66d82c2b4927e3794
Gustavo Silva <gustavo.silva@forkit.pt>
2018-08-25 10:48:04 +0100

    [new] header

--- ui/src/containers/Header/index.js
+++ ui/src/containers/Header/index.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-import './header.css';
+
+import BitcoinLogo from '../../assets/img/bitcoin-logo.svg';
+import Chip from '../../assets/img/chip.svg';
+
+import { Wrapper, Logo, DeviceFragment, DeviceTitle, Connected } from './styled';
 
 class Header extends Component {
+
+  // @dev - checks if the device is connected to the usb port
+  isDeviceConnected() {
+    // TODO: add function that checks if the device is connected
+    return true;
+  }
+
   render() {
+    const isConnected = this.isDeviceConnected();
     return (
-      <header>
-        <p>This is the Header container!</p>
-      </header>
+      <Wrapper>
+        <Logo
+          src={BitcoinLogo}
+        />
+        <DeviceFragment>
+          <DeviceTitle>
+            <img src={Chip} />
+            <span>Device</span>
+          </DeviceTitle>
+          <Connected
+            isConnected={isConnected}
+          >
+            {isConnected ? 'Connected' : 'Not connected'}
+          </Connected>
+        </DeviceFragment>
+      </Wrapper>
     );
   }
 }
---
+++ ui/src/containers/Header/styled.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.header`
+  height: 64px;
+  width: 100%;
+  display: inline-block;
+  background: white;
+`;
+
+export const Logo = styled.img`
+  float: left;
+  height: 30px;
+  width: 20px;
+  margin: 17px 30px;
+`;
+
+export const DeviceFragment = styled.div`
+  min-width: 200px;
+  height: 100%;
+  float: right;
+  padding-right: 30px;
+`;
+
+export const DeviceTitle = styled.div`
+  margin-top: 14px;
+  text-align: right;
+
+  >span {
+    width: 100%;
+    opacity: 0.5;
+    font-family: Lato;
+    font-size: 10px;
+    font-weight: bold;
+    letter-spacing: 0.1px;
+    color: #323c47;
+  }
+
+  >img {
+    margin-right: 6px;
+  }
+`;
+
+export const Connected = styled.div`
+  text-align: right;
+  color: ${(props) => props.isConnected ? '#0077ff' : 'red'}
+`;
